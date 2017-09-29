8d011c3f5b976c54f4c87c872434e58d8f5cdd24
stalintouch <stalintouch1983@gmail.com>
2017-09-29 13:21:11 -0400

    fix small bug

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -7,4 +7,4 @@ export const userLoggedIn = user => ({
 })
 
 
-export const login = (credentials) => (dispatch) => api.login(credentials).then(user => dispatch(userLoggedIn(user)));
+export const login = credentials => dispatch => api.user.login(credentials).then(user => dispatch(userLoggedIn(user)));
